cd45f9ad5027dd4bc4e7401c218578705d54303c
Marc Chino Altea <marcaltea2@gmail.com>
2025-08-23 20:30:51 +0800

    hero ui

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -14,11 +14,10 @@ const Hero = () => {
         />
       </div>
 
-      <div className="mx-auto max-w-2xl py-20 sm:py-48 lg:py-25">
+      <div className="mx-auto max-w-2xl py-15 sm:py-10 lg:py-20 ">
         <div className="text-center mb-4 sm:mb-5 sm:flex sm:justify-center">
-          <div className="relative rounded-full px-3 py-1 text-xl text-gray-600">
-            Hi, I am{" "}
-            <span className="font-semibold text-indigo-600">Chino</span>
+          <div className="px-3 py-1 text-xl text-gray-600 font-medium">
+            Hi, I am <span className="font-semibold text-primary">Chino</span>
           </div>
         </div>
 
@@ -26,16 +25,13 @@ const Hero = () => {
           <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
             Full-Stack Software Developer
           </h1>
-          <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+          <p className="mt-8 text-lg font-normal text-pretty text-gray-500 sm:text-xl/8">
             I turn ideas into user-friendly digital experiences, with 2 years of
             full-stack development focused on clean design and smooth
             functionality.
           </p>
           <div className="mt-10 flex items-center justify-center gap-x-6">
-            <a
-              href="#"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
+            <a href="#" className="btn btn-primary">
               My Projects
             </a>
             <a href="#" className="text-sm/6 font-semibold text-gray-900">
