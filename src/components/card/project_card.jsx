1cff50225b0428028cccf1ed7cd4f076cfacf338
Marc Chino Altea <marcaltea2@gmail.com>
2025-08-25 18:08:28 +0800

    padding x

--- src/components/card/project_card.jsx
+++ src/components/card/project_card.jsx
@@ -7,8 +7,8 @@ const Project = () => {
   const projects = ProjectsData();
 
   return (
-    <div className="w-full px-4 sm:px-6 lg:px-20 py-10">
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="w-full px-4 sm:px-6 lg:px-12 py-10">
+      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => (
           <div
             key={project.ID}
