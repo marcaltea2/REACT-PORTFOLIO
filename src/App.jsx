8789bf0df6e87ad1280ecc9b5e57e43a960cecf1
Marc Chino Altea <marcaltea2@gmail.com>
2025-08-25 18:06:17 +0800

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Project from "./pages/Project.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
 
 function App() {
   return (
     <>
-      <MainLayout>
-        <Home />
-      </MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Project/:Slug" element={<Project />}></Route>
+      </Routes>
     </>
   );
 }
