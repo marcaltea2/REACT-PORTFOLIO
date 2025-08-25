c3946892bc521daf95c990a96e080da3ebbfa02c
Marc Chino Altea <marcaltea2@gmail.com>
2025-08-25 19:01:38 +0800

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
-import Project from "./pages/Project.jsx";
-import MainLayout from "./layouts/MainLayout.jsx";
+import ProjectDetails from "./pages/ProjectDetails.jsx";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/Project/:Slug" element={<Project />}></Route>
+        <Route path="/Project/:Slug" element={<ProjectDetails />}></Route>
       </Routes>
     </>
   );
